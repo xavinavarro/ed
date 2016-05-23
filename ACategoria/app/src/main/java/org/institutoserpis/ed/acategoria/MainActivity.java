49835858d0ed17cb8ebbc2a00a9ad244542dfbb2
xavinavarro <dam.xnavarro@institutoserpis.org>
2016-05-23 12:16:44 +0200

    ACategoria
    Categoria

--- ACategoria/app/src/main/java/org/institutoserpis/ed/acategoria/MainActivity.java
+++ ACategoria/app/src/main/java/org/institutoserpis/ed/acategoria/MainActivity.java
@@ -2,6 +2,8 @@ package org.institutoserpis.ed.acategoria;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ListView listview = (ListView) findViewById(R.id.listView);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                new String[] {"Uno", "Dos"}
+        );
+        listview.setAdapter(arrayAdapter);
     }
 }
